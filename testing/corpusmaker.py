88aa57bd850eaba79ee346b966efb300cdbab2d8
ZAdammac <zachary.j.adam@gmail.com>
2017-12-26 22:48:25 -0400

    Adds the new corpusmaker, derived from an old snippit I wrote. This version is tailored for use with tapestry and uses text from the words file to try and have a half a shot at allowing compression to work.
    Signed-off-by: ZAdammac <zachary.j.adam@gmail.com>

---
+++ testing/corpusmaker.py
@@ -0,0 +1,33 @@
+# Just a simple tool for making a corpus of compressible files of some given bounds.
+# Properly maintained outside this repo as a scriptlet, but included here for clarity
+# This version was tweaked specifically for use with Tapestry. If desired, the general solution can be provided.
+# This script is not multiplatform; it's dependant on linux.
+
+import os.path
+import random
+
+global dest; dest = "/home/patches/Desktop/HASH/Tapestry Tests/Control/Corpus" # some pathlike destination to be root
+global num; num = 10 #Number of files to include per category
+global sizeEach; sizeEach = 10240 # Desired file size in lines (~10k/Gb)
+global subdirs; subdirs = ["documents", "photos", "music", "videos"]
+
+if not os.path.exists(dest):
+	os.mkdir(dest)
+os.chdir(dest)
+
+src = open("/usr/share/dict/words", "r").read().splitlines()
+bound = len(src)
+
+for subdir in subdirs:
+    if not os.path.exists(os.path.join(dest, subdir)):
+        os.mkdir(os.path.join(dest,subdir))
+    os.chdir(os.path.join(dest, subdir))
+    for i in range(1, num):
+        with open(str(i), "w") as file:
+            for line in range (1, sizeEach):
+                global line
+                line = str(" ")
+                for i in range (1, 12):
+                    line = line+str(src[random.randint(0, bound)])+" "
+                file.write(line)
+            file.close()
