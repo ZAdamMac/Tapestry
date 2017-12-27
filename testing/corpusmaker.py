70c09f884d1e98937003a612106724e17eb31809
ZAdammac <zachary.j.adam@gmail.com>
2017-12-27 07:43:44 -0400

    changed corpusmaker's settings to the tapestry testing standard.
    Signed-off-by: ZAdammac <zachary.j.adam@gmail.com>

--- testing/corpusmaker.py
+++ testing/corpusmaker.py
@@ -6,9 +6,9 @@
 import os.path
 import random
 
-global dest; dest = "/home/patches/Desktop/HASH/Tapestry Tests/Control/Corpus" # some pathlike destination to be root
+global dest; dest = "/home/patches/Desktop/Tapestry Testbed/Control/Corpus" # some pathlike destination to be root
 global num; num = 10 #Number of files to include per category
-global sizeEach; sizeEach = 10240 # Desired file size in lines (~10k/Gb)
+global sizeEach; sizeEach = 1000000 # Desired file size in lines (~1klines/Gb)
 global subdirs; subdirs = ["documents", "photos", "music", "videos"]
 
 if not os.path.exists(dest):
@@ -16,18 +16,18 @@ if not os.path.exists(dest):
 os.chdir(dest)
 
 src = open("/usr/share/dict/words", "r").read().splitlines()
-bound = len(src)
+bound = len(src)-1
 
 for subdir in subdirs:
     if not os.path.exists(os.path.join(dest, subdir)):
         os.mkdir(os.path.join(dest,subdir))
     os.chdir(os.path.join(dest, subdir))
-    for i in range(1, num):
+    for i in range(1, num+1):
         with open(str(i), "w") as file:
-            for line in range (1, sizeEach):
+            for line in range (0, sizeEach):
                 global line
                 line = str(" ")
-                for i in range (1, 12):
+                for i in range (0, 12):
                     line = line+str(src[random.randint(0, bound)])+" "
                 file.write(line)
             file.close()
