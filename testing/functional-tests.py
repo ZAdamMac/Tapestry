8ee3a44a269f2e9db17920c8eff9203f61888372
zadammac <zachary.j.adam@gmail.com>
2018-02-04 22:51:01 -0400

    Defines a rough outline of the functional testing script.
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

---
+++ testing/functional-tests.py
@@ -0,0 +1,28 @@
+#  Functional Testing Script for Tapestry versions 1.0 and later
+#  For full commentary and documentation view TESTDOCS.md in the repo.
+
+#  Import Modules
+
+#  Stash classes and functions here if necessary.
+
+#  Parse test config and build absolutized strings and things
+
+#  Establish a Logger for Test Output
+
+#  Do the bulk runs and context switching to generate the test outputs
+
+#  Identity Testing
+    #  Hash to Hash of Corpuses
+
+#  Encryption and Signing Passing
+    # Test if Signatures are Valid
+    # If Identity failed, test encryption
+
+#  Version Specificity
+    # If Identity failed, compare a test pickle to control pickle.
+    # Report the diff (pass if no diff)
+
+#  Compression Testing
+   # Test that output is smaller than Blocksize
+
+#  Clear Down!
