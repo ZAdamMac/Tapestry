5f75c0f8c958c81cabeac41021f3c2b533d66c09
zadammac <zachary.j.adam@gmail.com>
2018-02-04 23:49:38 -0400

    Clarifies some notes in functional-tests.py and adds TESTDOCS.md
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- testing/functional-tests.py
+++ testing/functional-tests.py
@@ -2,14 +2,25 @@
 #  For full commentary and documentation view TESTDOCS.md in the repo.
 
 #  Import Modules
+import configparser as cp
+import datetime as dt
+import gnupg
+import logging
+import os
+import shutil
+import unittest
 
 #  Stash classes and functions here if necessary.
 
 #  Parse test config and build absolutized strings and things
+cfg = cp.ConfigParser()
+cfg.read("tapestry-test.cfg")
+
+
 
 #  Establish a Logger for Test Output
 
-#  Do the bulk runs and context switching to generate the test outputs
+#  Do the bulk runs and context switching to generate the test outputs (make sure to seperate outputs between runs!)
 
 #  Identity Testing
     #  Hash to Hash of Corpuses
@@ -23,6 +34,9 @@
     # Report the diff (pass if no diff)
 
 #  Compression Testing
-   # Test that output is smaller than Blocksize
+    # Test that output is smaller than Blocksize
+
+#  Export Check
+    # Check for output!
 
 #  Clear Down!
