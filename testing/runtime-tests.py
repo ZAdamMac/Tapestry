a2fc949628c5c14c22e1f453b9ddde9a7a0de889
zadammac <zachary.j.adam@gmail.com>
2018-09-17 17:33:33 -0300

    Switches a read to binary mode to avoid encoding hassles.
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- testing/runtime-tests.py
+++ testing/runtime-tests.py
@@ -68,7 +68,7 @@ log = simpleLogger(logs, logname)
 log.log("------------------------------[SAMPLE GENERATION]------------------------------")
 log.log("\nThis log is for a test of a development version of Tapestry, with SHA256 hash:")
 hasher = hashlib.sha256()
-hasher.update(open("dev.py", "r").read())
+hasher.update(open("dev.py", "rb").read())
 taphash = hasher.hexdigest()
 log.log("\n"+str(taphash)+"\n")
 
