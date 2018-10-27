8f3fd82a0068615bd676bf35e36813ca60ad59d5
zadammac <zachary.j.adam@gmail.com>
2018-10-27 16:51:56 -0300

    PEP-8 Linting Fixes the call to SimpleLogger. Fixes some typos.
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- Development/Testing/runtime-tests.py
+++ Development/Testing/runtime-tests.py
@@ -13,30 +13,30 @@ import time
 
 
 def runtime():
-    permaHome = os.getcwd()
+    perma_home = os.getcwd()
 
     cfg = cp.ConfigParser()
     cfg.read("tapestry-test.cfg")
     out = cfg.get("Environment Variables", "output path")
     uid = cfg.get("Environment Variables", "uid")
     host = cfg.get("Environment Variables", "compID")
-    test_FTP_user = cfg.get("Network Configuration", "username")
-    logs = os.path.join(permaHome, "Logs")
-    blockSize = cfg.get("Environment Variables", "blocksize")
+    test_ftp_user = cfg.get("Network Configuration", "username")
+    logs = os.path.join(perma_home, "Logs")
+    block_size = cfg.get("Environment Variables", "blocksize")
 
-    shutil.copy("tapestry-test.cfg", "tapestry-test.cfg.bak") # We create a backup of the config to restore to after testing.
+    shutil.copy("tapestry-test.cfg", "tapestry-test.cfg.bak")
 
-    pathControl = out.replace("Test", "Control")
+    path_control = out.replace("Test", "Control")
 
     #  Establish a Logger for Test Output
     if not os.path.isdir(logs):
         os.mkdir(logs)
 
     logname = ("runtime_test-%s-%s.log" % (uid, str(date.today())))
-    log = framework.simpleLogger(logs, logname)
+    log = framework.SimpleLogger(logs, logname, "runtime-tests")
 
-
-    #  Do the bulk runs and context switching to generate the test outputs (make sure to seperate outputs between runs!)
+    # Do the bulk runs and context switching to generate the test outputs
+    # (make sure to seperate outputs between runs!)
     log.log("------------------------------[SAMPLE GENERATION]------------------------------")
     log.log("\nThis log is for a test of a development version of Tapestry, with SHA256 hash:")
     hasher = hashlib.sha256()
@@ -80,7 +80,8 @@ def runtime():
     cfg.set("Default Locations/Nix", "photos", pics.replace("Control", "Test"))
     vids = cfg.get("Additional Locations/Nix", "video")
     cfg.set("Additional Locations/Nix", "video", vids.replace("Control", "Test"))
-    cfg.remove_option("Additional Locations/Nix", "Music") # This should still wind up in corpus if you didn't break directionless recovery.
+    cfg.remove_option("Additional Locations/Nix", "Music")
+    # This should still wind up in corpus if you didn't break directionless recovery.
     with open("tapestry-test.cfg", "w") as warp:
         cfg.write(warp)
 
@@ -89,7 +90,7 @@ def runtime():
     waiting = subprocess.run(("python3.6", "dev.py", ("--rcv", "--devtest")))
     elapse = framework.elapsed(start)
     print("--rcv completed in %s" % elapse)
-    log.log("ecovery Mode Test Completed in %s - Returned:" % elapse)
+    log.log("Recovery Mode Test Completed in %s - Returned:" % elapse)
     log.log("%s" % waiting)
 
     shutil.copy("tapestry-test.cfg.bak", "tapestry-test.cfg")
