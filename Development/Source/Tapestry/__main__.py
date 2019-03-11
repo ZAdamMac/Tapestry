852377eef1ddf054130f2acbb231218557d2d1c9
zadammac <zachary.j.adam@gmail.com>
2019-03-11 14:44:21 -0300

    Fixes a bug where we forgot queues are queues and not lists.
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- Development/Source/Tapestry/__main__.py
+++ Development/Source/Tapestry/__main__.py
@@ -576,7 +576,7 @@ def pack_blocks(sizes, ops_list, namespace):
                                    str(datetime.date.today()), None, ops_list, ns.drop)
             this_task = tapestry.TaskTarBuild(tarf, "recovery-riff",
                                               this_riff, this_block_lock, locks)
-            tarf_queue.append(this_task)
+            tarf_queue.put(this_task)
 
         sum_jobs = len(tarf_queue)
         done = mp.JoinableQueue()
