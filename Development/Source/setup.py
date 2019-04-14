a4b6578892c515661a46a16767169c1214851996
zadammac <zachary.j.adam@gmail.com>
2019-04-14 17:47:47 -0300

    Adds a trivial setup.py script to the development build directory to facilitate building out test versions of the module.

---
+++ Development/Source/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tapestrydev',
+      version='2.0.0.devbuild.05',
+      description='tapestry development build - not suitable for release',
+      author='Zac Adam-MacEwen',
+      author_email='zadammac@kenshosec.com',
+      url='https://www.github.com/zadammac/Tapestry',
+      packages=['tapestrydev'],
+     )
