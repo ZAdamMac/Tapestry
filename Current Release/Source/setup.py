c67a69ec4074d8a053e0dcf74346c113c4279c1d
zadammac <zachary.j.adam@gmail.com>
2019-04-23 21:08:47 -0300

    It was necessary to do a post release to fix some metadata issues in PyPi.
    Also, the original push to master for these items did not include the actual source at Current Release/Source, so that's been rectified.
    
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- Current Release/Source/setup.py
+++ Current Release/Source/setup.py
@@ -4,12 +4,13 @@ with open("readme.md", "r") as f:
       long_description = f.read()
 
 setup(name='tapestry',
-      version='2.0.0',
+      version='2.0.0post1',
       description='Tapestry Bespoke Backup Utility',
       long_description=long_description,
       author='Zac Adam-MacEwen',
       author_email='zadammac@kenshosec.com',
       url='https://www.github.com/zadammac/Tapestry',
       packages=['tapestry'],
-      long_description_format="text/markdown"
+      dependancies=['python-gnupg'],
+      long_description_content_type="text/markdown"
      )
