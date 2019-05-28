83e9ee62f381ad2d869c30371e1efc50a89608f6
zadammac <zachary.j.adam@gmail.com>
2019-05-28 20:37:32 -0300

    Completely rewrites corpusmaker.py: - platform agnostic (no more reliance on the dictionary files. - random enough to make hash collisions between generated files unlikely. - new random seed from os.urandom for each word. - accepts command line arguments.
    Tested in place, works as intended. Signing off.
    
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- Development/Testing/corpusmaker.py
+++ Development/Testing/corpusmaker.py
@@ -1,33 +1,74 @@
-# Just a simple tool for making a corpus of compressible files of some given bounds.
-# Properly maintained outside this repo as a scriptlet, but included here for clarity
-# This version was tweaked specifically for use with Tapestry. If desired, the general solution can be provided.
-# This script is not multiplatform; it's dependant on linux.
+"""
+A simple script to generate a small, structured corpus of files. Improves upon
+the original by accepting CLI arguments and being platform agnostic.
+"""
 
-import os.path
+
+import argparse
+import os
 import random
+from string import printable as characters
 
-global dest; dest = "/home/patches/Desktop/Tapestry FT Data/Control/Corpus" # some pathlike destination to be root
-global num; num = 10 #Number of files to include per category
-global sizeEach; sizeEach = 1000000 # Desired file size in lines
-global subdirs; subdirs = ["documents", "photos", "music", "videos"]
+__version__ = "2.0.0"
 
-if not os.path.exists(dest):
-	os.mkdir(dest)
-os.system("""echo "this is a test" > testblock.txt""")
-os.chdir(dest)
 
-src = open("/usr/share/dict/words", "r").read().splitlines()
-bound = len(src)-1
+def parse_args():
+    """Parse the arguments given to the script/module at runtime for later use.
+    
+    :return: Tuple of arguments set to variables for use later in the script.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', help="absolute or relative path to the top level of the corpus", action="store")
+    parser.add_argument('-p', help="number of files to generate in each directory", action="store")
+    parser.add_argument('-d', help="comma-delimited string defining a list of directories to create to structure the data", action="store")
+    parser.add_argument('-l', help="length of each file, in words. Noisy but direct correlation to overall file size", action="store")
 
-for subdir in subdirs:
-    if not os.path.exists(os.path.join(dest, subdir)):
-        os.mkdir(os.path.join(dest,subdir))
-    os.chdir(os.path.join(dest, subdir))
-    for i in range(1, num+1):
-        with open(str(i), "w") as file:
-            for line in range (0, sizeEach):
-                line = str(" ")
-                for i in range (0, 12):
-                    line = line+str(src[random.randint(0, bound)])+" "
-                file.write(line)
-            file.close()
+    args = parser.parse_args()
+    root_path = args.r
+    file_count_per_folder = int(args.p)
+    directory_list = args.d.split(",")
+    file_length = int(args.l)
+
+    return root_path, file_count_per_folder, directory_list, file_length
+
+
+def create_folder_heirarchy(dir_list):
+    for each in dir_list:
+        if not os.path.exists(each):
+            os.mkdir(each)
+    print("Done creating directories.")
+
+
+def populate_corpus_files(files_count, file_length):
+    print("Beginning corpus file generation. This could take a while.")
+    files_spawned = 0
+    for a, extant_dirs, files in os.walk(os.getcwd()):
+        total_files = files_count * len(dirs)
+        for current_dir in extant_dirs:
+            for each in range(0, files_count):
+                files_spawned += 1
+                print("Now generating file %s of %s" % (files_spawned, total_files))
+                file_name = os.path.join(current_dir, str(files_spawned))
+                generate_file(file_name, file_length)
+    print("Done generating files. The script will now exit.")
+    exit(0)
+
+
+def generate_file(name, length):
+    with open(name, "w") as f:
+        for each in range(0, length+1):
+            word_length = random.randint(5, 20)
+            word = ""
+            for char in range(word_length):  # build a word out of random characters
+                random.seed(os.urandom(256))
+                next_char = random.choice(characters)
+                word += next_char
+                f.write(word)
+
+
+if __name__ == "__main__":
+    root, per_folder, dirs, file_words = parse_args()
+    print("Corpusmaker %s is now deployed, targeting the %s directory" % (__version__, root))
+    os.chdir(root)
+    create_folder_heirarchy(dirs)
+    populate_corpus_files(per_folder, file_words)
