222f171dfb0fc5b82e8817f6d7ce70544db2d11b
Zachary Adam-MacEwen <zadammac@kenshosec.com>
2019-10-04 13:43:52 -0300

    positive_tests.py: - re-references a test incorrectly enumerated in list_local_tests, correcting a condition where the existing test would never have been called (and the errant reference would likely have caused an uncaught exception)
    Signed-off-by: zadammac <zachary.j.adam@gmail.com>

--- Development/Testing/tests/positive_tests.py
+++ Development/Testing/tests/positive_tests.py
@@ -74,7 +74,7 @@ def runtime(dict_config, do_network):
     # Populate this list with all tests to be run locally.
     list_local_tests = [test_block_valid_put, test_block_yield_full, test_block_meta,
                         test_riff_find, test_riff_compliant, test_pkl_find,
-                        test_TaskCheckIntegrity, test_TaskCompress, test_TaskDecompress,
+                        test_TaskCheckIntegrity_call, test_TaskCompress, test_TaskDecompress,
                         test_TaskEncrypt, test_TaskDecrypt, test_TaskSign,
                         test_TaskTarBuild, test_TaskTarUnpack, test_build_ops_list,
                         test_build_recovery_index, test_media_retrieve_files,
