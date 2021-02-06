97f42a41846ccf68a391d98e5070d8dd1ea0be6f
zadammac <zadammac@kenshosec.com>
2021-02-05 20:50:11 -0400

    Commit adds: - IDE vcs configuration file because I'm tired of rebuilding it. - adds keyring as a dependency on the development source. - updates versioning in development/source/setup.py to indicate latest development effort.

--- Development/Source/setup.py
+++ Development/Source/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='tapestry',
-      version='2.1.1.20201230',
+      version='2.2.0.keyring.01',
       description='tapestry development build - not suitable for release',
       author='Zac Adam-MacEwen',
       author_email='zadammac@kenshosec.com',
       url='https://www.github.com/zadammac/Tapestry',
       packages=['tapestry'],
-      install_requires=['pysftp', 'python-gnupg', 'paramiko'],
+      install_requires=['pysftp', 'python-gnupg', 'paramiko', 'keyring'],
       )
